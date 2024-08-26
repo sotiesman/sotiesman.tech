1c1a5288aa55f9f811292e158f9f83b1da119c32
Nazar <sotiesman@noreply.codeberg.org>
2024-08-26 20:47:53 +0300

    feat: add not-found page

--- src/components/Spotify.svelte
+++ src/components/Spotify.svelte
@@ -1,101 +1,99 @@
 <script>
-	import {
-		SPOTIFY_CLIENT_ID,
-		SPOTIFY_CLIENT_SECRET,
-		SPOTIFY_REFRESH_TOKEN,
-	} from "astro:env/client";
-	import { Buffer } from "buffer";
-	import { onMount } from "svelte";
+  import {
+    SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET,
+    SPOTIFY_REFRESH_TOKEN,
+  } from "astro:env/client";
+  import { Buffer } from "buffer";
+  import { onMount } from "svelte";
 
-	const CLIENT_ID = SPOTIFY_CLIENT_ID;
-	const CLIENT_SECRET = SPOTIFY_CLIENT_SECRET;
-	const REFRESH_TOKEN = SPOTIFY_REFRESH_TOKEN;
+  const CLIENT_ID = SPOTIFY_CLIENT_ID;
+  const CLIENT_SECRET = SPOTIFY_CLIENT_SECRET;
+  const REFRESH_TOKEN = SPOTIFY_REFRESH_TOKEN;
 
-	const BASIC = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
-	const NOW_PLAYING_ENDPOINT =
-		"https://api.spotify.com/v1/me/player/currently-playing";
-	const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
+  const BASIC = Buffer.from(`${CLIENT_ID}:${CLIENT_SECRET}`).toString("base64");
+  const NOW_PLAYING_ENDPOINT =
+    "https://api.spotify.com/v1/me/player/currently-playing";
+  const TOKEN_ENDPOINT = "https://accounts.spotify.com/api/token";
 
-	const getAccessToken = async () => {
-		const response = await fetch(TOKEN_ENDPOINT, {
-			method: "POST",
-			headers: {
-				Authorization: `Basic ${BASIC}`,
-				"Content-Type": "application/x-www-form-urlencoded",
-			},
-			body: new URLSearchParams({
-				grant_type: "refresh_token",
-				refresh_token: REFRESH_TOKEN,
-			}),
-		});
+  const getAccessToken = async () => {
+    const response = await fetch(TOKEN_ENDPOINT, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${BASIC}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        grant_type: "refresh_token",
+        refresh_token: REFRESH_TOKEN,
+      }),
+    });
 
-		const data = await response.json();
+    const data = await response.json();
 
-		return data.access_token;
-	};
+    return data.access_token;
+  };
 
-	let isPlaying = false,
-		songUrl = null,
-		name = null,
-		artist = null;
+  let isPlaying = false,
+    songUrl = null,
+    name = null,
+    artist = null;
 
-	const last_scrobble = async () => {
-		const accessToken = await getAccessToken();
+  const last_scrobble = async () => {
+    const accessToken = await getAccessToken();
 
-		console.log(import.meta.env);
+    const response = await fetch(NOW_PLAYING_ENDPOINT, {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
-		const response = await fetch(NOW_PLAYING_ENDPOINT, {
-			headers: {
-				Authorization: `Bearer ${accessToken}`,
-			},
-		});
+    if (response.status === 204) {
+      isPlaying = false;
+      songUrl = null;
+      name = null;
+      artist = null;
 
-		if (response.status === 204) {
-			isPlaying = false;
-			songUrl = null;
-			name = null;
-			artist = null;
+      return;
+    }
 
-			return;
-		}
+    const song = await response.json();
 
-		const song = await response.json();
+    isPlaying = song.is_playing;
+    songUrl = song.item.external_urls.spotify;
+    name = song.item.name;
+    artist = song.item.artists.map((artist) => artist);
+  };
 
-		isPlaying = song.is_playing;
-		songUrl = song.item.external_urls.spotify;
-		name = song.item.name;
-		artist = song.item.artists.map(artist => artist);
-	};
-
-	onMount(() => {
-		setInterval(() => last_scrobble(), 1000);
-	});
+  onMount(() => {
+    setInterval(() => last_scrobble(), 1000);
+  });
 </script>
 
 {#if isPlaying !== false}
-	<!-- svelte-ignore missing-declaration -->
-	<span transition:scale={{}}>
-		<a href={songUrl}>{name.length > 30 ? `${name.substring(0, 30)}…` : name}</a
-		>
-		<span>{" / "}</span>
-		{#each artist as artist}
-			<a href={artist.external_urls.spotify}>{artist.name}</a>
-			{#if artist.length === 1}
-				<span>{" / "}</span>
-			{/if}
-		{/each}
-	</span>
+  <!-- svelte-ignore missing-declaration -->
+  <span>
+    <a href={songUrl}>{name.length > 30 ? `${name.substring(0, 30)}…` : name}</a
+    >
+    <span>{" / "}</span>
+    {#each artist as artist}
+      <a href={artist.external_urls.spotify}>{artist.name}</a>
+      {#if artist.length === 1}
+        <span>{" / "}</span>
+      {/if}
+    {/each}
+  </span>
 {/if}
 
 <style lang="scss">
-	@use "../styles/_thorns.scss";
+  @use "../styles/_thorns.scss";
 
-	a {
-		color: thorns.$fg0;
+  a {
+    color: thorns.$fg0;
 
-		&:hover,
-		&:focus {
-			text-decoration: unset;
-		}
-	}
+    &:hover,
+    &:focus {
+      text-decoration: unset;
+    }
+  }
 </style>
