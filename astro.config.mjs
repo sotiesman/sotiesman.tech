17914c90eaf4f6c4797ef5f21513bfd57f6daea2
Nazar <sotiesman@noreply.codeberg.org>
2024-08-26 19:46:01 +0300

    add redirect

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,24 +7,27 @@ import { defineConfig, envField } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://sotiesman.tech",
-	integrations: [svelte(), mdx(), sitemap(), robots(), compress()],
-	experimental: {
-		env: {
-			schema: {
-				SPOTIFY_CLIENT_ID: envField.string({
-					context: "client",
-					access: "public",
-				}),
-				SPOTIFY_CLIENT_SECRET: envField.string({
-					context: "client",
-					access: "public",
-				}),
-				SPOTIFY_REFRESH_TOKEN: envField.string({
-					context: "client",
-					access: "public",
-				}),
-			},
-		},
-	},
+  site: "https://sotiesman.tech",
+  integrations: [svelte(), mdx(), sitemap(), robots(), compress()],
+  redirects: {
+    "/git": "https://codeberg.org/sotiesman",
+  },
+  experimental: {
+    env: {
+      schema: {
+        SPOTIFY_CLIENT_ID: envField.string({
+          context: "client",
+          access: "public",
+        }),
+        SPOTIFY_CLIENT_SECRET: envField.string({
+          context: "client",
+          access: "public",
+        }),
+        SPOTIFY_REFRESH_TOKEN: envField.string({
+          context: "client",
+          access: "public",
+        }),
+      },
+    },
+  },
 });
